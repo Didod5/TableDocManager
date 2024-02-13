3aefb4fcf01342a4dad9c98c266724d4a11cfe53
Stepanov Dmitry <Didod1020@gmail.com>
2024-02-13 19:37:31 +0400

    added more forms

--- people.py
+++ people.py
@@ -20,10 +20,10 @@ class People(QScrollArea):
         self.label_0 = QLabel('Люди')
         self.add_widget_to_grid(self.label_0, 0, 0, 1, 3, alignment=Qt.AlignCenter)
 
-        self.label_1 = QLabel('Представитель застройщика (технического заказчика, эксплуатирующей организации\n или регионального оператора) по вопросам строительного контроля')
-        self.label_2 = QLabel('В случае осуществления строительства, реконструкции,\nкапитального ремонта на основании договора строительного подряда')
-        self.add_widget_to_grid(self.label_1, 1, 0, 1, 3, alignment=Qt.AlignCenter)
-        self.add_widget_to_grid(self.label_2, 2, 0, 1, 3, alignment=Qt.AlignCenter)
+        self.label_1 = QLabel('\nПредставитель застройщика (технического заказчика, эксплуатирующей организации\n или регионального оператора) по вопросам строительного контроля')
+        self.label_2 = QLabel('(В случае осуществления строительства, реконструкции,\nкапитального ремонта на основании договора строительного подряда)')
+        self.add_widget_to_grid(self.label_1, 1, 1, 1, 3, alignment=Qt.AlignBottom)
+        self.add_widget_to_grid(self.label_2, 2, 1, 1, 3, alignment=Qt.AlignBottom)
 
         self.layout.addWidget(QLabel('Должность'), 3, 0, alignment=Qt.AlignRight)
         self.layout.addWidget(QLabel('Фамилия И. О.'), 4, 0, alignment=Qt.AlignRight)
@@ -32,15 +32,15 @@ class People(QScrollArea):
         self.layout.addWidget(QLabel('Наименование, ОГРН, ИНН, место\n нахождения юридического \nлица (В случае осуществления \nстроительного контроля на основании \nдоговора с застройщиком, техническим \nзаказчиком, эксплуатирующей организацией\n или региональным оператором)'),
                                 7, 0, alignment=Qt.AlignRight)
         
-        self.label_3 = QLabel('Представитель лица, осуществляющего строительство')
-        self.add_widget_to_grid(self.label_3, 8, 0, 2, 3, alignment=Qt.AlignCenter)
+        self.label_3 = QLabel('\nПредставитель лица, осуществляющего строительство')
+        self.add_widget_to_grid(self.label_3, 8, 1, 2, 3, alignment=Qt.AlignBottom)
         self.layout.addWidget(QLabel('Должность'), 10, 0, alignment=Qt.AlignRight)
         self.layout.addWidget(QLabel('Фамилия И. О.'), 11, 0, alignment=Qt.AlignRight)
         self.layout.addWidget(QLabel('Реквизиты распорядительного\nдокумента, подтверждающего\nполномочия'), 12, 0, alignment=Qt.AlignRight)
         self.layout.addWidget(QLabel('Наименование юридического лица'), 13, 0, alignment=Qt.AlignRight)
 
-        self.label_4 = QLabel('Представитель лица, осуществляющего строительство,\nпо вопросам строительного контроля (специалист по \nорганизации строительства)')
-        self.layout.addWidget(self.label_4, 14, 0, 2, 3, alignment=Qt.AlignCenter)
+        self.label_4 = QLabel('\nПредставитель лица, осуществляющего строительство, по вопросам\nстроительного контроля (специалист по организации строительства)')
+        self.layout.addWidget(self.label_4, 14, 1, 2, 3, alignment=Qt.AlignBottom)
         self.layout.addWidget(QLabel('Должность'), 16, 0, alignment=Qt.AlignRight)
         self.layout.addWidget(QLabel('Фамилия И. О.'), 17, 0, alignment=Qt.AlignRight)
         self.layout.addWidget(QLabel('Идентификационный номер\nспециалиста в НРС и дата'), 18, 0, alignment=Qt.AlignRight)
@@ -48,15 +48,45 @@ class People(QScrollArea):
         self.layout.addWidget(QLabel('Наименование юридического лица'), 20, 0, alignment=Qt.AlignRight)
         
         self.label_5 = QLabel('\nПредставитель лица, осуществляющего подготовку проектной документации\n(в случаях когда авторский надзор осуществляется)')
-        self.layout.addWidget(self.label_5, 21, 0, 2, 3, alignment=Qt.AlignCenter)
+        self.layout.addWidget(self.label_5, 21, 1, 2, 3, alignment=Qt.AlignBottom)
         self.layout.addWidget(QLabel('Должность'), 23, 0, alignment=Qt.AlignRight)
         self.layout.addWidget(QLabel('Фамилия И. О.'), 24, 0, alignment=Qt.AlignRight)
         self.layout.addWidget(QLabel('Реквизиты распорядительного документа,\nподтверждающего полномочия'),
                               25, 0, alignment=Qt.AlignRight)
-        self.layout.addWidget(QLabel('Наименование, ОГРН, ИНН, место нахождения\n юридического лица (В случае \nосуществления авторского надзора\nлицом, не являющимся разработчиком \nпроектной документации)'),
+        self.layout.addWidget(QLabel('Наименование, ОГРН, ИНН, место нахождения\nюридического лица (В случае\nосуществления авторского надзора\nлицом, не являющимся разработчиком\nпроектной документации)'),
                               26, 0, alignment=Qt.AlignRight)
-        self.layout.addWidget(QLabel('Наименование, ОГРН, ИНН саморегулируемой \nорганизации, членом которой является\n указанное юридическое лицо \n(За исключением случаев, когда членство в \nсаморегулируемых организациях в области \nархитектурно-строительного проектирования \nне требуется)',),
+        self.layout.addWidget(QLabel('Наименование, ОГРН, ИНН саморегулируемой\nорганизации, членом которой является\n указанное юридическое лицо\n(За исключением случаев, когда членство в\nсаморегулируемых организациях в области\nархитектурно-строительного проектирования\nне требуется)',),
                               27, 0, alignment=Qt.AlignRight)
+        
+        self.label_6 = QLabel('\nПредставитель лица, выполнившего работы, подлежащие освидетельствованию\n(в случае выполнения работ по договорам о строительстве, реконструкции,\nкапитальном ремонте объектов капитального строительства, заключенным\nс иными лицами)')
+        self.layout.addWidget(self.label_6, 28, 1, 2, 3, alignment=Qt.AlignBottom)
+        self.layout.addWidget(QLabel('Должность'), 30, 0, alignment=Qt.AlignRight)
+        self.layout.addWidget(QLabel('Фамилия И. О.'), 31, 0, alignment=Qt.AlignRight)
+        self.layout.addWidget(QLabel('Реквизиты распорядительного документа,\nподтверждающего полномочия'), 32, 0, alignment=Qt.AlignRight)
+        self.layout.addWidget(QLabel('Наименование, ОГРН, ИНН, место\nнахождения юридического лица'), 33, 0, alignment=Qt.AlignRight)
+
+        self.label_7 = QLabel('\nПредставитель лица, выполнившего работы, подлежащие освидетельствованию\nДля АОУСИТО')
+        self.layout.addWidget(self.label_7, 34, 1, 2, 3, alignment=Qt.AlignBottom)
+        self.layout.addWidget(QLabel('Должность'), 36, 0, alignment=Qt.AlignRight)
+        self.layout.addWidget(QLabel('Фамилия И. О.'), 37, 0, alignment=Qt.AlignRight)
+
+        self.label_8 = QLabel('\nПредставитель иного лица 1')
+        self.layout.addWidget(self.label_8, 38, 1, 2, 3, alignment=Qt.AlignBottom)
+        self.layout.addWidget(QLabel('Должность с указанием\nнаименования организации'), 40, 0, alignment=Qt.AlignRight)
+        self.layout.addWidget(QLabel('Фамилия И. О.'), 41, 0, alignment=Qt.AlignRight)
+        self.layout.addWidget(QLabel('Реквизиты распорядительного документа,\nподтверждающего полномочия'), 42, 0, alignment=Qt.AlignRight)
+        
+        self.label_9 = QLabel('\nПредставитель иного лица 2')
+        self.layout.addWidget(self.label_9, 43, 1, 2, 3, alignment=Qt.AlignBottom)
+        self.layout.addWidget(QLabel('Должность с указанием\nнаименования организации'), 45, 0, alignment=Qt.AlignRight)
+        self.layout.addWidget(QLabel('Фамилия И. О.'), 46, 0, alignment=Qt.AlignRight)
+        self.layout.addWidget(QLabel('Реквизиты распорядительного документа,\nподтверждающего полномочия'), 47, 0, alignment=Qt.AlignRight)
+
+        self.label_10 = QLabel('\nПредставитель иного лица 3')
+        self.layout.addWidget(self.label_10, 48, 1, 2, 3, alignment=Qt.AlignBottom)
+        self.layout.addWidget(QLabel('Должность с указанием\nнаименования организации'), 50, 0, alignment=Qt.AlignRight)
+        self.layout.addWidget(QLabel('Фамилия И. О.'), 51, 0, alignment=Qt.AlignRight)
+        self.layout.addWidget(QLabel('Реквизиты распорядительного документа,\nподтверждающего полномочия'), 52, 0, alignment=Qt.AlignRight)
 
         # 2 column
         self.construction_supervisor_job = QLineEdit()
@@ -101,7 +131,41 @@ class People(QScrollArea):
         self.layout.addWidget(self.project_documentation_rep_company, 26, 1) 
         self.layout.addWidget(self.project_documentation_rep_full_company, 27, 1)
         
+        self.completed_work_representative_job = QLineEdit()
+        self.completed_work_representative_name = QLineEdit()
+        self.completed_work_representative_requisites = QPlainTextEdit()
+        self.completed_work_representative_company = QPlainTextEdit()
+        self.layout.addWidget(self.completed_work_representative_job, 30, 1)
+        self.layout.addWidget(self.completed_work_representative_name, 31, 1)
+        self.layout.addWidget(self.completed_work_representative_requisites, 32, 1)
+        self.layout.addWidget(self.completed_work_representative_company, 33, 1)
 
+        self.completed_work_representative_AOUSITO_job = QLineEdit()
+        self.completed_work_representative_AOUSITO_name = QLineEdit()
+        self.layout.addWidget(self.completed_work_representative_AOUSITO_job, 36, 1)
+        self.layout.addWidget(self.completed_work_representative_AOUSITO_name, 37, 1)
+
+        self.other_entity_1_job = QLineEdit()
+        self.other_entity_1_name = QLineEdit()
+        self.other_entity_1_requisites = QLineEdit()
+        self.layout.addWidget(self.other_entity_1_job, 40, 1)
+        self.layout.addWidget(self.other_entity_1_name, 41, 1)
+        self.layout.addWidget(self.other_entity_1_requisites, 42, 1)
+
+        self.other_entity_2_job = QLineEdit()
+        self.other_entity_2_name = QLineEdit()
+        self.other_entity_2_requisites = QLineEdit()
+        self.layout.addWidget(self.other_entity_2_job, 45, 1)
+        self.layout.addWidget(self.other_entity_2_name, 46, 1)
+        self.layout.addWidget(self.other_entity_2_requisites, 47, 1)
+
+        self.other_entity_3_job = QLineEdit()
+        self.other_entity_3_name = QLineEdit()
+        self.other_entity_3_requisites = QLineEdit()
+        self.layout.addWidget(self.other_entity_3_job, 50, 1)
+        self.layout.addWidget(self.other_entity_3_name, 51,1)
+        self.layout.addWidget(self.other_entity_3_requisites, 52, 1)        
+        
         # 3 column
         self.layout.addWidget(QLabel('Что получилось:'), 3, 2)
 
